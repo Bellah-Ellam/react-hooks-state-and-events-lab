595e12710fa8ba6c9eb5a39d2a2e4ea7c8620981
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-04-25 13:45:32 +0300

    Complete asignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  const [buttonText, setButtonText] = useState("Add to Cart");
+
+  function handleClick() {
+    if (inCart) {
+      setInCart(false);
+      setButtonText("Add to Cart");
+    } else {
+      setInCart(true);
+      setButtonText("Remove From Cart");
+    }
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>
+        {buttonText}
+      </button>
     </li>
   );
 }
